cf323dff6e2a18eaf082d92a91dae41b1b36165c
recep-sen <devrecepsen@gmail.com>
2022-12-25 18:34:53 +0300

    Cube is now static instance

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     [SerializeField] private float speed;
-    // Start is called before the first frame update
-    void Start()
+    public static Cube Instance;
+    void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(Time.deltaTime * speed,0f , 0f);
